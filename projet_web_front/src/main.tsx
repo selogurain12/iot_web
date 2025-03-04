377bafc255482b15afa34c829ca116586c54fbba
Lora Seguin <lora80123@gmail.com>
2025-03-04 14:45:13 +0100

    feat(smartsecure/webui): add toast in main

--- projet_web_front/src/main.tsx
+++ projet_web_front/src/main.tsx
@@ -3,7 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { AuthentificationIndex } from './components/authentification';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { ListUsers } from './components/users_list';
+import { ListUsers } from './components/utilisateurs/users_list';
+import { Toaster } from 'sonner';
 
 const container = document.querySelector("#root");
 
@@ -12,6 +13,7 @@ const root = createRoot(container!);
 root.render(
   <StrictMode>
     <Router>
+    <Toaster richColors />
       <Routes>
         <Route path="/" element={<AuthentificationIndex />} />
         <Route path="/userlist" element={<ListUsers />} />
