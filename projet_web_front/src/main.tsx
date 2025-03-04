4120ccbc2de77159f90b548309cbb8b6c4654d88
Lora Seguin <lora80123@gmail.com>
2025-03-04 11:15:27 +0100

    feat(smartsecure/webui): add user page - #3

--- projet_web_front/src/main.tsx
+++ projet_web_front/src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { AuthentificationIndex } from './components/authentification';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ListUsers } from './components/users_list';
 
 const container = document.querySelector("#root");
 
@@ -9,6 +11,11 @@ const root = createRoot(container!);
 
 root.render(
   <StrictMode>
-    <AuthentificationIndex />
+    <Router>
+      <Routes>
+        <Route path="/" element={<AuthentificationIndex />} />
+        <Route path="/userlist" element={<ListUsers />} />
+      </Routes>
+  </Router>
   </StrictMode>,
 )
